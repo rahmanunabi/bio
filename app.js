e0e9c25123f0fdb4ddbc2f214103e26c078e68ad
rahmanunabi <102538438+rahmanunabi@users.noreply.github.com>
2022-05-24 16:38:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var userName = prompt ("Please enter User Name");
+
+alert("My name is" + " " + userName)
+var age = prompt ("enter your age?");
+alert("I am" + " " + age + " " + "years old..!");
+var institute = prompt ("Institute Name");
+alert("I study in" + " " + institute);
+var course = prompt ("enrolled course");
+alert("I take" + " " + course);
+alert("I have a gift for you")
+var emailId = 
+alert("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P");
+
+if (userName === userName){
+    alert("welcome" + "\n" + userName + "\n" + "Please enter your password!");    
+}
+else{
+alert("Error! Please enter a valid User Name.");
+}
+var password = prompt ("Please enter your password" + " " + "Hint (Pakistani)");
+
+if (password === "Pakistani"){
+    alert(welcome + "\n" + happycode);    
+}
+else{
+    alert("Error! Please enter a valid password.");
+    }
